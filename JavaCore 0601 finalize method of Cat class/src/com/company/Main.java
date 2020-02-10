09678d8a810a2dc0b55242e2070eec3f622e39bc
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-10 13:12:12 +0200

    Add files via upload

---
+++ JavaCore 0601 finalize method of Cat class/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+
+/*
+
+0601 Метод finalize класса Cat
+Метод finalize класса Cat
+В классе Cat создать метод protected void finalize() throws Throwable.
+
+Требования:
+1. В классе Cat должен быть метод с именем finalize.
+2. Метод finalize ничего не должен возвращать (void).
+3. Метод finalize должен быть protected.
+4. Метод finalize может кидать исключение Throwable, укажи это при объявлении метода.
+
+*/
+
+// public class Solution
+public class Main {
+//    public static void main(String[] args) throws Exception {
+      protected void finalize() throws Throwable {
+    }
+}
+
+
+
+
+
+
+
